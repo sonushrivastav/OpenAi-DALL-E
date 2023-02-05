59323c9f7cc1041d4248feb91f0d56eb372e92a5
Atomos Software Development Engineering <sonushrivastav1811@gmail.com>
2023-02-05 22:54:17 +0530

    commiting changes

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ const app = express();
 import path from "path";
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
-
 const __dirname = path.dirname(__filename);
 const PORT = 4000 || process.env.PORT
-import connectDB from "../mongodb/connection.js"; 
+import connectDB from "./mongodb/connection.js"; 
 dotenv.config();
-import postRoutes from "../routes/postRoutes.js"
-import dalleRoutes from "../routes/dalleRoutes.js.js"
+import postRoutes from "./routes/postRoutes.js"
+import dalleRoutes from "./routes/dalleRoutes.js"
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }))

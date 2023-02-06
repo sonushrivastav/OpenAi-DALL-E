4f834981666336a6f97bd682bfd9a6ad155caa3f
Atomos Software Development Engineering <sonushrivastav1811@gmail.com>
2023-02-06 21:28:39 +0530

    commiting changes

--- server/index.js
+++ server/index.js
@@ -3,14 +3,14 @@ import * as dotenv from "dotenv"
 import cors from "cors"
 const app = express();
 import path from "path";
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// import { fileURLToPath } from 'url';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 const PORT =  process.env.PORT || 4000
-import connectDB from "./mongodb/connection.js"; 
+import connectDB from "../mongodb/connection.js"; 
 dotenv.config();
-import postRoutes from "./routes/postRoutes.js"
-import dalleRoutes from "./routes/dalleRoutes.js"
+import postRoutes from "../routes/postRoutes.js"
+import dalleRoutes from "../routes/dalleRoutes.js"
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }))
@@ -18,18 +18,18 @@ app.use(express.json({ limit: "50mb" }))
 app.use("/api/v1/post",postRoutes)
 app.use("/api/v1/dalle", dalleRoutes)
 
-// serving the frontend
-app.use(express.static(path.join(__dirname, "../client/dist")));
+// // serving the frontend
+// app.use(express.static(path.join(__dirname, "../client/dist")));
 
 
-app.get("*", function (req, res) {
-    res.sendFile(
-        path.join(__dirname, "../client/dist/index.html"),
-        function (err) {
-            res.status(500).send(err)
-        }
-    )
-})
+// app.get("*", function (req, res) {
+//     res.sendFile(
+//         path.join(__dirname, "../client/dist/index.html"),
+//         function (err) {
+//             res.status(500).send(err)
+//         }
+//     )
+// })
 
 app.get("/", async (req, res) => {
     res.send("hello from DAll-E!")

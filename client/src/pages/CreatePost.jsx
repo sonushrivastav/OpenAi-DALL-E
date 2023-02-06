4206cbe00cce08b9a5b2af908d267e782a82aa68
Atomos Software Development Engineering <sonushrivastav1811@gmail.com>
2023-02-06 22:37:55 +0530

    commiting changes

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -48,7 +48,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch(`http://localhost:4000/api/v1/dalle`, {  
+        const response = await fetch(`https://open-ai-dall-e-api.vercel.app/api/v1/dalle`, {  
           method: "POST",
           headers: { "Content-Type": "application/json", },
           body:JSON.stringify({prompt:form.prompt,})
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
       setLoading(true)
 
       try {
-        const response = await fetch(`http://localhost:4000/api/v1/post`, {
+        const response = await fetch(`https://open-ai-dall-e-api.vercel.app/api/v1/post`, {
           method: "GET",
           headers: {
             "Content-Type":"application/json",

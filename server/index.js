99e7bf2699fad594be0d60fb7285da01a45ba5b0
Atomos Software Development Engineering <sonushrivastav1811@gmail.com>
2023-02-06 22:24:39 +0530

    commiting changes

--- server/index.js
+++ server/index.js
@@ -2,15 +2,15 @@ import express  from "express";
 import * as dotenv from "dotenv"
 import cors from "cors"
 const app = express();
-import path from "path";
+// import path from "path";
 // import { fileURLToPath } from 'url';
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 const PORT =  process.env.PORT || 4000
-import connectDB from "../mongodb/connection.js"; 
+import connectDB from "./mongodb/connection.js"; 
 dotenv.config();
-import postRoutes from "../routes/postRoutes.js"
-import dalleRoutes from "../routes/dalleRoutes.js"
+import postRoutes from "./routes/postRoutes.js"
+import dalleRoutes from "./routes/dalleRoutes.js"
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }))
